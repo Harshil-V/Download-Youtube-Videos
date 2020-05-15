0833341d12d4625026ce4f2514ed5d28f23aa3fd
Harshil-V <harshil@ualberta.ca>
2020-05-15 14:07:52 -0600

    Fixed video audio issue and and created an exe file to run program

--- Youtube_video_downloader.py
+++ Youtube_video_downloader.py
@@ -9,21 +9,18 @@ print("\n")
 print(link_title)
 print("\n")
 
-Download_format = input("DO YOU WANT TO DOWNLOAD A VIDEO OR AUDIO" "\n" "Enter V for video or A for audio (V / A): ")
+Download_format = input("DO YOU WANT TO DOWNLOAD A VIDEO OR AUDIO" "\n" "Enter V for video or A for audio (V / A): ""\n")
 
 
 if(Download_format == "V" or Download_format == "v"):
     print("Here Are All The Video Files" "\n")
-    video_type = Youtube_Connection.streams.filter(type = 'video')
+    video_type = Youtube_Connection.streams.filter(subtype='mp4', progressive=True)
     for stream in video_type:   
         print(str(index) + " " + str(stream))
         index += 1
     
     User_download_index = int(input("Enter the Format Number That You Want To Download: "))
     downloading_File = video_type[User_download_index - 1]
-
-    
-print("\n")
 
 if(Download_format == "A" or Download_format == "a"):
     print("Here Are All The Audio Files" "\n")
@@ -42,9 +39,9 @@ print(downloading_File)
 download_request = input("DO YOU WANT TO DOWNLOAD THIS FILE (Y / N): " "\n")
 
 if(download_request == 'y' or download_request == 'Y'):
-    print("Your Video or Audio is Being Downloaded" "\n")
+    print("Your Video or Audio is Being Downloaded......" "\n")
     downloading_File.download() 
     print("The File has been downloaded")
 else:
-    print("The Program will End.") 
+    print("The Program Ended.") 
 
